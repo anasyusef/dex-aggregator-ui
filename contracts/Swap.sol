046689cd0bcc2c61c5e4d0aeb312864d2dbe913c
anasyusef <anas.y0807@gmail.com>
2022-03-09 19:17:06 +0000

    feat: :construction: implement router interface

--- contracts/Swap.sol
+++ contracts/Swap.sol
@@ -3,71 +3,134 @@ pragma solidity ^0.8.0;
 import "./interfaces/IUniswapV2Pair.sol";
 import "./libraries/UniswapV2Library.sol";
 import "./interfaces/IUniswapV2Router02.sol";
+import "./interfaces/IWETH.sol";
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Swap {
+contract Swap is Ownable {
     event Received(address, uint256);
-    IUniswapV2Router02 public immutable router;
+    // IUniswapV2Router02 public immutable router;
+    address public immutable WETH;
 
-    constructor(address _router) {
-        router = IUniswapV2Router02(_router);
+    // Add the library methods
+    using EnumerableMap for EnumerableMap.UintToAddressMap;
+
+    // Declare a set state variable
+    EnumerableMap.UintToAddressMap private routers;
+
+    constructor(address _WETH) {
+        WETH = _WETH;
     }
 
-    function swap(
-        address _tokenIn,
-        address _tokenOut,
-        uint256 _amountIn,
-        uint256 _amountOutMin,
-        address _to
-    ) external {
-        IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
-        IERC20(_tokenIn).approve(address(router), _amountIn);
-
-        address[] memory path;
-
-        if (_tokenIn == router.WETH() || _tokenOut == router.WETH()) {
-            path = new address[](2);
-            path[0] = _tokenIn;
-            path[1] = _tokenOut;
-        } else {
-            path = new address[](3);
-            path[0] = _tokenIn;
-            path[1] = router.WETH();
-            path[2] = _tokenOut;
-        }
-
-        router.swapExactTokensForTokens(
-            _amountIn,
-            _amountOutMin,
-            path,
-            _to,
-            block.timestamp
+    function aggregatePrices(uint256 amountIn, address[] memory path)
+        external
+        view
+        returns (uint256[] memory amounts)
+    {
+        IUniswapV2Router02 router = IUniswapV2Router02(routers.get(0));
+        amounts = UniswapV2Library.getAmountsOut(
+            router.factory(),
+            amountIn,
+            path
         );
+        console.log(amounts.length);
     }
 
-    function swapETHForExactTokens(
-        address _tokenOut,
-        uint256 _amountOutMin,
-        address _to
+    function swapExactETHForTokens(
+        uint256 _exchangeIdx,
+        uint256 amountOutMin,
+        address _beneficiary,
+        address[] calldata path,
+        uint256 deadline
     ) external payable returns (uint256[] memory amounts) {
-        // amountOutMin must be retrieved from an oracle of some kind
-        // console.log(_tokenOut, _amountOutMin, _to, msg.value);
-        address[] memory path = new address[](2);
-        path[0] = router.WETH();
-        path[1] = _tokenOut;
-        console.log(_amountOutMin);
-        amounts = router.swapETHForExactTokens{value: msg.value}(
-            _amountOutMin,
+        IUniswapV2Router02 router = getRouter(_exchangeIdx);
+        amounts = router.swapExactETHForTokens{value: msg.value}(
+            amountOutMin,
             path,
-            _to,
-            block.timestamp
+            _beneficiary,
+            deadline
         );
 
         console.log("Input: %s ETH; Output: %s DAI", amounts[0], amounts[1]);
         uint256 dust = msg.value - amounts[0];
-        if (dust > 0) payable(_to).transfer(dust);
+        if (dust > 0) payable(_beneficiary).transfer(dust);
     }
+
+    function getRouter(uint256 _exchangeIdx)
+        public
+        view
+        returns (IUniswapV2Router02)
+    {
+        (bool success, address routerAddress) = routers.tryGet(_exchangeIdx);
+        require(success, "Router not initialised");
+        return IUniswapV2Router02(routerAddress);
+    }
+
+    function addRouter(uint256 _exchangeIdx, address routerAddress)
+        external
+        onlyOwner
+        returns (bool success)
+    {
+        success = routers.set(_exchangeIdx, routerAddress);
+        require(success, "Failed to add router");
+    }
+
+    // function swap(
+    //     address _tokenIn,
+    //     address _tokenOut,
+    //     uint256 _amountIn,
+    //     uint256 _amountOutMin,
+    //     address _to
+    // ) external {
+    //     IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
+    //     IERC20(_tokenIn).approve(address(router), _amountIn);
+
+    //     address[] memory path;
+
+    //     if (_tokenIn == router.WETH() || _tokenOut == router.WETH()) {
+    //         path = new address[](2);
+    //         path[0] = _tokenIn;
+    //         path[1] = _tokenOut;
+    //     } else {
+    //         path = new address[](3);
+    //         path[0] = _tokenIn;
+    //         path[1] = router.WETH();
+    //         path[2] = _tokenOut;
+    //     }
+
+    //     router.swapExactTokensForTokens(
+    //         _amountIn,
+    //         _amountOutMin,
+    //         path,
+    //         _to,
+    //         block.timestamp
+    //     );
+    // }
+
+    // function swapETHForExactTokens(
+    //     address _tokenOut,
+    //     uint256 _amountOutMin,
+    //     address _to
+    // ) external payable returns (uint256[] memory amounts) {
+    //     // amountOutMin must be retrieved from an oracle of some kind
+    //     // console.log(_tokenOut, _amountOutMin, _to, msg.value);
+    //     address[] memory path = new address[](2);
+    //     path[0] = router.WETH();
+    //     path[1] = _tokenOut;
+    //     console.log(_amountOutMin);
+    //     amounts = router.swapETHForExactTokens{value: msg.value}(
+    //         _amountOutMin,
+    //         path,
+    //         _to,
+    //         block.timestamp
+    //     );
+
+    //     console.log("Input: %s ETH; Output: %s DAI", amounts[0], amounts[1]);
+    //     uint256 dust = msg.value - amounts[0];
+    //     if (dust > 0) payable(_to).transfer(dust);
+    // }
 
     receive() external payable {
         emit Received(msg.sender, msg.value);
--- test/index.ts
+++ test/index.ts
@@ -11,6 +11,7 @@ import {
   TradeType,
   WETH,
 } from "@uniswap/sdk";
+import UniswapRouterV2Abi from "../abis/UniswapV2Router02.json";
 
 const provider = waffle.provider;
 
@@ -68,13 +69,30 @@ describe("Swap", function () {
     // expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 
-  it("should swap from eth to token", async function () {
+  it.only("should swap from eth to token", async function () {
     const [owner] = await ethers.getSigners();
     const Swap = await ethers.getContractFactory("Swap");
     const swap = await Swap.deploy(
       "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
     );
     await swap.deployed();
+
+    const uniswapV2RouterAddress = ethers.utils.getAddress(
+      "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+    );
+    const sushiswapRouterAddress = ethers.utils.getAddress(
+      "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F"
+    );
+
+    const shibaswapRouterAddress = ethers.utils.getAddress(
+      "0x03f7724180AA6b939894B5Ca4314783B0b36b329"
+    );
+
+    await swap.addRouter(0, uniswapV2RouterAddress);
+    await swap.addRouter(1, sushiswapRouterAddress);
+    await swap.addRouter(2, shibaswapRouterAddress);
+
+    expect(await swap.getRouter(0)).to.equal(uniswapV2RouterAddress);
     const DAIAddress = ethers.utils.getAddress(
       "0x6B175474E89094C44Da98b954EedeAC495271d0F"
     );
@@ -90,19 +108,31 @@ describe("Swap", function () {
       new TokenAmount(WETH[DAI.chainId], amountIn.toBigInt()),
       TradeType.EXACT_INPUT
     );
+    const path = [WETH[DAI.chainId].address, DAI.address];
+    const deadline = Math.floor(Date.now() / 1000) + 120;
     const slippageTolerance = new Percent("50", "10000");
     const amountOutMin = trade.minimumAmountOut(slippageTolerance).raw;
-    const tx = await swap.swapETHForExactTokens(
-      DAI.address,
+    const tx = await swap.swapExactETHForTokens(
+      2,
       amountOutMin.toString(),
       owner.address,
+      path,
+      deadline,
       {
         value: amountIn,
       }
     );
     await tx.wait();
-    const contractBalance = await provider.getBalance(swap.address);
-    console.log(ethers.utils.formatEther(contractBalance));
+
+    const DAIAbi = new ethers.utils.Interface([
+      "function balanceOf(address account) external view returns (uint256)",
+    ]);
+    const DAIContract = new ethers.Contract(DAI.address, DAIAbi, owner);
+    console.log(
+      ethers.utils.formatUnits(await DAIContract.balanceOf(owner.address), 18)
+    );
+    // const contractBalance = await provider.getBalance(swap.address);
+    // console.log(ethers.utils.formatEther(contractBalance));
     console.log(
       `Owner's balance: ${ethers.utils.formatEther(await owner.getBalance())}`
     );
